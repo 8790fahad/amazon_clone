ce0e23fa110ff0228c41e77ea9dff5000553c1db
danjuma <dangana2011@hotmail.com>
2020-09-13 23:16:26 +0000

    changes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express =require('express')
+const stripe = require('stripe')('sk_test_51HR3PQHNRSN0XZEC5kvAqc44Bvl1FORjo2NUaITv7nq7x6uXSAXoohigKN52Wp5iUKdSPhuw6AP4upriNymQIVPf0088Q5o8U6')
+const app = express()
+app.use(cors({origin:true}))
+app.use(express.json())
+app.get('/',(req,res)=>{
+    req.status(200).send('hello')
+})
+exports.api=functions.https.onRequest(app)
